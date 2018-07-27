cec87de30fccb2ecd8fbf1e4ca925ce7b91ea8be
christianliu13 <christianliu13@gmail.com>
2018-07-26 17:09:40 -0700

    done v1

--- website/src/components/Nav.js
+++ website/src/components/Nav.js
@@ -8,6 +8,7 @@ export default class Nav extends React.Component {
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav nav-fill">
             <div id="nav-spacing" className="row container-fluid">
